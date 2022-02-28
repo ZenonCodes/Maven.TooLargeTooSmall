c7581673f62aa5a97920dce40de73e2c8c779d46
Vera <vera@Zipcoders-MBP.fios-router.home>
2022-02-28 11:06:00 -0500

    Created Guessing game and tested successfully

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,38 @@
+import java.util.*;
+import java.lang.*;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+
 public class Main {
 
+    public void GuessGame() {
+        Random random = new Random();
+        int rightGuess =random.nextInt(100);
+        Scanner scanner = new Scanner(System.in);
+        int guess;
+        do {
+            System.out.println("Guess a number between 1 - 100!");
+             guess = scanner.nextInt();
+            if(guess > rightGuess){
+                System.out.println("Too big!");
+            } else if (guess < rightGuess){
+                System.out.println("Too small!");
+            }
+        } while (guess != rightGuess);
+
+        System.out.println("Congratulations! You got it!!");
+
+
+    }
+
     public static void main(String[] args){
+        Main obj = new Main();
+        obj.GuessGame();
+
+
         
     }
 }
